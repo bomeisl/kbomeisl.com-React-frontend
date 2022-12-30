78acba20bd242368355fc14018e381f967cc570d
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:41:24 -0500

    stacking imports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Projects from './Projects';
 import Skills from './Skills';
+import Stack from 'react-bootstrap/Stack';
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
