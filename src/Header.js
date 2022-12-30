ada7949a04d6cb3488e608d7a1d71870c0c84f4c
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 15:34:58 -0500

    I think this is right...

--- src/Header.js
+++ src/Header.js
@@ -22,22 +22,39 @@ const Header = () => {
     const renderHeader = () => {
         return(
             <div className="ui three stackable raised cards">
-                {headerInfo.map(header => {
+                        <h1 class='ui header justify-content-center'>
+                                <Stack direction='vertical' gap={3}>
+                                    <h1 class="display 2 ">Kyle Bomeisl</h1>
+                                    <h2 class="display 4 text-muted">Software Engineer</h2>
+                                </Stack>  
 
+                        
+                        
+                        
+                                <Navbar bg="light" variant="light">
+                        
+                                <Container>
+                        
+                                <Navbar.Brand href="#home">Kyle's Workshop</Navbar.Brand>
+                                    <Nav className="me-auto">
+                                        <Nav.Link href="#home">About Me</Nav.Link>
+                                        <Nav.Link href="#features">My Projects</Nav.Link>
+                                        <Nav.Link href="#pricing">Musings (blog)</Nav.Link>
+                                        <Nav.Link href="#pricing">How This Website Works</Nav.Link>
+                                    </Nav>
+                        
+                                </Container>
+                                </Navbar>
+                        </h1>
+
+                {headerInfo.map(header => {
+                    
                     if(!header){
                         return <div>One moment please...</div>
 
                     } else {
                         return (
                             <div>
-                            <Stack direction='horizontal' gap={3}>
-                
-                            <h1 class='ui header justify-content-center'>
-                                <Stack direction='vertical' gap={3}>
-                                    <h1 class="display 2 ">Kyle Bomeisl</h1>
-                                    <h2 class="display 4 text-muted">Software Engineer</h2>
-                                </Stack>  
-                            </h1>
             
                             <Stack direction='horizontal' gap={1}>
                                 <Figure>
@@ -50,30 +67,13 @@ const Header = () => {
                             </Stack>
             
             
-                        </Stack>
-                        
-                        
-                        <Navbar bg="light" variant="light">
-                        
-                            <Container>
-                            
-                            <Navbar.Brand href="#home">Kyle's Workshop</Navbar.Brand>
-                            <Nav className="me-auto">
-                                <Nav.Link href="#home">About Me</Nav.Link>
-                                <Nav.Link href="#features">My Projects</Nav.Link>
-                                <Nav.Link href="#pricing">Musings (blog)</Nav.Link>
-                                <Nav.Link href="#pricing">How This Website Works</Nav.Link>
-                            </Nav>
-                            
-                            </Container>
-                        </Navbar>
-                        </div>
+                        </div>   
                                 
                         )
                     }
                 })}
-
             </div>
+          
         )
     }    
    
