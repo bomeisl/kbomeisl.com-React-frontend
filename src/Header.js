5674e6d05c709a1f5ac796be85c80883fe30c3e9
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 16:09:30 -0500

    resize icons

--- src/Header.js
+++ src/Header.js
@@ -61,7 +61,7 @@ const Header = () => {
                                 <Figure>
                                     <Figure.Image
                                         width={'12%'}
-                                        height={'11%'}
+                                        height={'9%'}
                                         alt={header.name}
                                         src={header.image}/>
                                 </Figure>        
