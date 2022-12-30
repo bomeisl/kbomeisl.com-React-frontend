9f652bce8d8ef89c7a4fb489d5c120c1c9624412
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 16:58:19 -0500

    intro final

--- src/Header.js
+++ src/Header.js
@@ -32,7 +32,8 @@ const Header = () => {
                                     <h6 class="display 7">You are viewing a screen rendered by a React frontend I built that is consuming a 
                                     Django REST API backend that I built as well. The static and media assets are served from a PostgreSQL database and a Microsoft
                                     Azure Blob Static Asset Storage Container that I created and configured. The backend and frontend are each hosted at 
-                                    different domains, so I configured CORS to allow requests between them.  
+                                    different domains, so I configured CORS to allow requests between them. Welcome to the world wide web of world wide
+                                    web development! And welcome to the landing screen of my portfolio web app!
                                     
                                     </h6>
                                 </Stack>                                  
