16c0d057ff08a9f87d0e4a6c9ebdbc5b4ca6745a
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:13:55 -0500

    bootstrap react rather than regular bootstrap

--- src/Header.js
+++ src/Header.js
@@ -9,25 +9,16 @@ const Header = () => {
             <h2 class="display 4 text-muted">Software Engineer</h2>
         </h1>
 
-        <nav class="navbar navbar-light" style="background-color: #e3f2fd;">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <a class="navbar-brand" href="#">Portfolio</a>
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">About Me<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Skill Set</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">My Musings (blog)</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+        <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       </div>
     )
 }
