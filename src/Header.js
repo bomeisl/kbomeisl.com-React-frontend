46a2366de20d6fcfb97dda00b12e0b6613d68293
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 01:51:28 -0500

    typography

--- src/Header.js
+++ src/Header.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Header = () => {
     return (
         <h1 class='ui header'>
-            <h1>Kyle Bomeisl</h1>
-            <h2>Software Engineer</h2>
+            <h1 class="display 2">Kyle Bomeisl</h1>
+            <h2 class="display 4">Software Engineer</h2>
         </h1>
     )
 }
