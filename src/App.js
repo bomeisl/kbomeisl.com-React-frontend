9ca3ea9815aee3e77532849ffe403584c5d21f3c
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:02:11 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Projects from './Projects';
 import Skills from './Skills';
+import NavBar from './NavBar'
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
      <Header />
+     <NavBar />
      <Skills />
      <Projects />
     </div>
