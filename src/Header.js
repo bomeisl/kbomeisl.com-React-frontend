a9b059263f444e235ee19651551ddef2749859c1
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:45:05 -0500

    card imports

--- src/Header.js
+++ src/Header.js
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Stack from 'react-bootstrap/Stack';
+import Card from 'react-bootstrap/Card';
 
 const Header = () => {
     return (
