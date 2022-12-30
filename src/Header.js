74fe35d52cde2d2b8f5fcdcc37262160c32d5ef9
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:42:22 -0500

    stacking imports

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Stack } from 'react-bootstrap';
+import Stack from 'react-bootstrap/Stack';
 
 const Header = () => {
     return (
