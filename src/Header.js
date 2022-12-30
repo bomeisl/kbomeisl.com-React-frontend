3c6dffa5d3ad7330aa863a79c1da35c2dc377a09
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 02:15:46 -0500

    bootstrap imports

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
     return (
