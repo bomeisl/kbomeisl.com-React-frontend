8080683bc9804d54c6be893179a5f5f6ea005fb9
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-30 18:03:31 -0500

    well intro

--- src/Header.js
+++ src/Header.js
@@ -30,13 +30,14 @@ const Header = () => {
                                     <h1 class="display 1 ">Kyle Bomeisl</h1>
                                     <h2 class="display 3 text-muted">Software Engineer</h2>
                                     <h6 class="display 7">
+                                    <Well>
                                     Behind every great Web app is a great RESTful API with CRUD operations. This one is no exception of course.
                                     You are viewing a screen rendered by a React frontend I built that is consuming a 
                                     Django RESTful API backend with a PostgreSQL database that I lovingly designed and programmed as well. The static and media assets are served from a 
                                     Azure Blob Static Asset Storage Container that I created and configured. The backend and frontend are each hosted at 
                                     different domains, so I configured CORS to allow requests between them. Welcome to the world wide web of world wide
                                     web development! And welcome to the landing screen of my portfolio web app!
-                                    
+                                    </Well>
                                     </h6>
                                 </Stack>                                  
                         </h1>                       
