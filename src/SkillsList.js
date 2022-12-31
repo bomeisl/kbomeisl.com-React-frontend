dd65719e05ce2f92ab36281104e12e74b0a5be91
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-31 11:47:17 -0500

    remove () from map

--- src/SkillsList.js
+++ src/SkillsList.js
@@ -3,7 +3,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 const SkillsList = (props) => {
 
-    const skillList = (props.cluster).map((skill)=> 
+    const skillList = props.cluster.map(skill=> 
         <ListGroup.Item>{skill.language}</ListGroup.Item>
     );
 
