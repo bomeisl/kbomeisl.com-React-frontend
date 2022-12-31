9cc9894b21375dc65adee6134650770fac9061e9
Kyle Bomeisl <kaybomeisl2@gmail.com>
2022-12-31 02:13:09 -0500

    add home page

---
+++ src/HomePage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Home = () => {
+
+
+    return (
+        <h6 class="display 7" id="intro">
+                                    Behind every great Web app is a great RESTful API with CRUD operations. This one is no exception of course.
+                                    You are viewing a screen rendered by a React frontend I built that is consuming a 
+                                    Django RESTful API backend with a PostgreSQL database that I lovingly designed and programmed as well. The static and media assets are served from a 
+                                    Azure Blob Static Asset Storage Container that I created and configured. The backend and frontend are each hosted at 
+                                    different domains, so I configured CORS to allow requests between them. Welcome to the world wide web of world wide
+                                    web development! And welcome to the landing screen of my portfolio web app!
+                                    <br></br><br></br>
+                                    
+                                    
+                                    </h6>
+    )
+    
+}
+
+export default Home;
